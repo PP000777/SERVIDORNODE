7b0cff253f68d346dd259ef2e3dc2886ae5d5c25
PP000777 <bia.silva007@hotmail.com>
2025-04-29 17:34:26 -0300

    adicionando função de get e tbm o conteudo de dentro do nosso banco de dados né

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser')
 const express = require('express')
 const fs = require('fs')
 const app = express()
@@ -11,3 +12,25 @@ function salvarBanco(dados, res, resposta) {
         res.status(200).json(resposta)
     })
 }
+
+app.get('/aulas', (req, res) => {
+    fs.readFile('bancodeDados.json', JSON.stringify(dados, null, 4), (err) => {
+        if (err) return res.status(500).json({ msg: "Erro ao ler o arquivo" })
+        const dados = JSON.parse(data)
+        res.status(200).json(JSON.parse(data))
+    })
+})
+
+app.get('/aulas/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
+        if (err) return res.status(500).json({ msg: "Erro ao ler o banco de dados" })
+
+        const dados = JSON.parse(data)
+        const aula = dados.find(item => item.id === id)
+
+        if (!aula) return res.status(404).json({ msg: "Aula não encontrada" })
+        res.status(200).json(aula)
+    })
+})
